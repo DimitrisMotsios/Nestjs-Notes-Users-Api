a05af6a2fb62d3414ac503f23c4b138255517085
Motsios <www.motsios@hotmail.com>
2021-01-22 22:40:26 +0200

    Commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { Connection } from 'typeorm';
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "",
+    password: "m19941994",
     database: "noteApp",
     entities: [__dirname+"/**/*entity{.ts,.js}"],
     synchronize: true}),
